e5e282ebe1b5acd104179fd015ea4be500c30433
bobermilk <bobermilk@users.noreply.github.com>
2022-08-29 16:52:04 +0800

    setup: dont force win

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from cx_Freeze import setup, Executable
 build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
 
 # base="Win32GUI" should be used only for Windows GUI app
-base = "Win32GUI"
+base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
